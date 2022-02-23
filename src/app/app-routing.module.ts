ff9f5bf4b5cdd50974cc02c726e689b1a462014d
mayur savaliya <mayursavaliya696@gmail.com>
2022-02-23 23:15:55 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./mycomponents/home/home.component";
+import {ToolsComponent} from "./mycomponents/tools/tools.component";
+import {AboutComponent} from "./mycomponents/about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tools', component: ToolsComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './mycomponents/home/home.component';
+import { HeaderComponent } from './mycomponents/header/header.component';
+import { FooterComponent } from './mycomponents/footer/footer.component';
+import { ToolsComponent } from './mycomponents/tools/tools.component';
+import { AboutComponent } from './mycomponents/about/about.component';
+import { LoginComponent } from './mycomponents/login/login.component';
+import { SignupComponent } from './mycomponents/signup/signup.component';
+import { ContactComponent } from './mycomponents/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ToolsComponent,
+    AboutComponent,
+    LoginComponent,
+    SignupComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
